c2ecc63b87966d339189f266ce22259c6e68de40
1zzzttt1 <2453115025@qq.com>
2026-03-28 21:13:22 +0800

    feat: 使用lenis，丝滑滚动

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
 
 const app = createApp(App)
 
@@ -11,3 +14,10 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const lenis = new Lenis({
+  autoRaf: true,
+  duration: 1.1,
+  smoothWheel: true,
+  touchMultiplier: 1.2,
+})
